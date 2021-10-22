6565933489d68a0b608ccfa1aacbb1516fed93b3
Mikhail Petrov <mikhail.petrov@math.msu.ru>
2021-10-22 22:01:36 +0300

    tried to fix sorting bug

--- IndexSort/selectionSort.c
+++ IndexSort/selectionSort.c
@@ -8,19 +8,20 @@ void selectionSort(int *mass, int len, int * index) {
     int iter = 0; 
     int change; // Вспомогательная переменная для обмена
     int min;
-    int ind = 0;
+    int min_ind;
 
     while (iter < len) {
+        min_ind = 0;
         min = mass[iter];
         for (int i = iter; i < len; i++) {
             if (mass[index[i]] < min) {
                 min = mass[index[i]];
-                ind = i;
+                min_ind = i;
             }
         }
-        change = iter;
-        index[iter] = ind;
-        index[ind] = change;
+        change = index[iter];
+        index[iter] = index[min_ind];
+        index[min_ind] = change;
         // printf("%d -> %d\n", ind, change); // отладочная печать
         iter++;
     }
